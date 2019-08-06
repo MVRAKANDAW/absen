cddcf71ed1d092b8cdccfb29ad1bd606be6dd68d
MVRAKANDAW <mochammadvianto@gmail.com>
2019-08-06 09:24:54 +0700

    Update

---
+++ resources/views/FrontEnd/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    @include('frontend.layout.header')
+</head>
+<body>   
+        <div class="content-body"> 
+            @yield('content')
+        </div>
+</body>
+</html>
